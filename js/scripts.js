d9842ad8b45044797fce1aa1015e003bd465731b
Steven Polanco <6752411+spolanco94@users.noreply.github.com>
2022-06-30 01:15:19 -0400

    Include scroll arrow on landing, general styling changes + navbar
    Chevron arrow added with bouncing animation and 'scroll' prompt to
    direct users to scroll down. Animation added to chevron.
    Background color changed to a gradient with height adjusted to allow
    seamless design.
    Navbar stylings added, with a script that controls when it will appear;
    around the edge of the about and landing sections.

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,17 @@ $(document).ready(function() {
   let $body = $('body'),
       $nav = $('.navbar'),
       $window = $(window),
-      navOffsetTop = $nav.offset().top,
+      navOffsetTop = $('.navbar-spacer').offset().top,
       $document = $(document);
   
   $window.on('scroll', function(){
     if(navOffsetTop < $window.scrollTop() && !$body.hasClass('has-docked-nav')){
       $body.addClass('has-docked-nav');
+      $nav.css("display", "block");
     }
     if(navOffsetTop > $window.scrollTop() && $body.hasClass('has-docked-nav')){
       $body.removeClass('has-docked-nav');
+      $nav.css("display", "none");
     }
   });
 });
