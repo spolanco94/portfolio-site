2fecf0b63ffe809d77966a37175b54de6f15899e
Steven Polanco <6752411+spolanco94@users.noreply.github.com>
2022-07-04 22:46:22 -0400

    Include three projects, load scripts asynchronously

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,17 @@ $(document).ready(function() {
   let $body = $('body'),
       $nav = $('.navbar'),
       $window = $(window),
-      navOffsetTop = $('.navbar-spacer').offset().top,
+      $navOffsetTop = $('.navbar-spacer').offset().top,
       $document = $(document);
   
   $window.on('scroll', function(){
-    if(navOffsetTop < $window.scrollTop() && !$body.hasClass('has-docked-nav')){
+    console.log("nav: " + $navOffsetTop);
+    console.log("window: " + $window.scrollTop());
+    if($navOffsetTop < $window.scrollTop() && !$body.hasClass('has-docked-nav')){
       $body.addClass('has-docked-nav');
       $nav.css("display", "block");
     }
-    if(navOffsetTop > $window.scrollTop() && $body.hasClass('has-docked-nav')){
+    if($navOffsetTop > $window.scrollTop() && $body.hasClass('has-docked-nav')){
       $body.removeClass('has-docked-nav');
       $nav.css("display", "none");
     }
