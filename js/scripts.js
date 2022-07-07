b4bf122ed8ef75ed2ae2115b4bf6ce23aea79be8
Steven Polanco <6752411+spolanco94@users.noreply.github.com>
2022-07-06 22:50:32 -0400

    Create success page for contact form, rewrite navbar script, add alt text for skills icons

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,29 @@
-$(document).ready(function() {
-  let $body = $('body'),
-      $nav = $('.navbar'),
-      $window = $(window),
-      $navOffsetTop = $('.navbar-spacer').offset().top,
-      $form = $('#contact-me')
-      $document = $(document);
-  
-  $window.on('scroll', function(){
-    if($navOffsetTop < $window.scrollTop() && !$body.hasClass('has-docked-nav')){
-      $body.addClass('has-docked-nav');
-      $nav.css("display", "block");
+function docReady(fn) {
+  // see if DOM is already available
+  if (document.readyState === "complete" || document.readyState === "interactive") {
+      // call on next available tick
+      setTimeout(fn, 1);
+  } else {
+      document.addEventListener("DOMContentLoaded", fn);
+  }
+}    
+
+docReady(function(){
+  let body = document.body,
+      nav = document.getElementById('navbar'),
+      navOffset = document.getElementById('navSpacer').offsetTop;
+
+  window.addEventListener('scroll', function(){
+    if(navOffset < document.documentElement.scrollTop && !body.classList.contains('has-docked-nav')){
+      body.classList.toggle('has-docked-nav');
+      nav.style.display = 'block';
+      console.log("on");
     }
-    if($navOffsetTop > $window.scrollTop() && $body.hasClass('has-docked-nav')){
-      $body.removeClass('has-docked-nav');
-      $nav.css("display", "none");
+    if(navOffset > document.documentElement.scrollTop && body.classList.contains('has-docked-nav')){
+      body.classList.toggle('has-docked-nav');
+      nav.style.display = 'none';
+      console.log("off");
     }
   });
-
-});
-
-const form = document.getElementById('contact-me');
-
-document.addEventListener('DOMContentLoaded', function() {
-  console.log('Document Ready');
 });
 
